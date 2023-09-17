911971de42fa77990645d02ff34ed7f8281eff9a
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2023-09-17 00:38:22 -0300

    Create Calculadora Estatística - CRUD.py

---
+++ Calculadora Estatística - CRUD.py
@@ -0,0 +1,11 @@
+########################################################
+#EST: Análise de Pareto, Medidas de Tendência Central e Histograma
+#Atividade avaliativa
+#Disciplina de Estatistica Aplicada
+#Programador : Rafael da Silva Brito
+#Professor: Luiz Carlos dos Santos Filho
+########################################################
+
+#Importação de Bibliotecas
+import os
+
